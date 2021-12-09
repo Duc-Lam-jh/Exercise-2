213a795df388b78f87285162105ff6d068967cd0
duclam227 <duc.lam@journeyh.io>
2021-12-09 17:43:18 +0700

    updated after review

--- map/map.js
+++ map/map.js
@@ -1,46 +1,148 @@
-// import { MapboxStyleSwitcherControl } from "mapbox-gl-style-switcher";
-// import "mapbox-gl-style-switcher/styles.css";
-
 mapboxgl.accessToken =
 	'pk.eyJ1IjoiZHVjbGFtMjI3IiwiYSI6ImNrd3Z6OHhqZDA4a3cyb3M4czltcHAwZXMifQ.5bneNUlldaEBHRv9vr0vNA';
 
-//get user's current location, if success, call function successLocation, otherwise call errorLocation
-navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
-	enableHighAccuracy : true,
+const map = new mapboxgl.Map({
+	container : 'map',
+	style     : 'mapbox://styles/mapbox/streets-v11',
 });
+
+const markers = [];
+let navigate = false;
+loadMap();
+
+// Add zoom and rotation controls to the map.
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav);
 
 //when successfully got user's location, zoom in on it on the map
 function successLocation (position) {
-	//console.log(position);
-	setUpMap([ position.coords.longitude, position.coords.latitude ]);
+	flyToPlace([ position.coords.longitude, position.coords.latitude ]);
 }
 
 //if cannot get user's location, zoom into the default coords
-function errorLocation () {
-	setUpMap([ 0, 0 ]);
+function errorLocation () {}
+
+function flyToPlace (center) {
+	map.flyTo({
+		center    : center,
+		essential : true,
+	});
+
+	if(!navigate){
+		markers.forEach(function (item) {
+			item.remove();
+		});
+		markers.length = 0;
+	}	
+	const marker = new mapboxgl.Marker().setLngLat(center).addTo(map);
+	markers.push(marker);
 }
 
-function setUpMap (center) {
-	const map = new mapboxgl.Map({
-		container : 'map',
-		style     : 'mapbox://styles/mapbox/streets-v11',
-		center    : center,
-		zoom      : 17,
+function loadMap () {
+	//get user's current location, if success, call function successLocation, otherwise call errorLocation
+	navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+		enableHighAccuracy : true,
 	});
 
-	// Add zoom and rotation controls to the map.
-	const nav = new mapboxgl.NavigationControl();
-	map.addControl(nav);
-
-	const geocoder = new MapboxGeocoder({
-		accessToken : mapboxgl.accessToken,
-		mapboxgl    : mapboxgl
+	const searchbox = document.getElementById('searchbox');
+	searchbox.addEventListener('keyup', function () {
+		SearchForPlace('');
 	});
-  map.addControl(geocoder, 'top-left');
 
-  const styleswitcher = new MapboxStyleSwitcherControl();
-  map.addControl(styleswitcher);
+	document.addEventListener('click', function (e) {
+		if (
+			e.target != document.getElementById('searchbox') ||
+			e.target != document.getElementById('search-results')
+		) {
+			if (!document.getElementById('search-results').classList.contains('none')) {
+				document.getElementById('search-results').classList.add('none');
+			}
+		};
 
-  // const language = new MapboxLanguage();
-	// map.addControl(language);
+		if (
+			e.target != document.getElementById('second-searchbox') ||
+			e.target != document.getElementById('second-search-results')
+		) {
+			if (!document.getElementById('second-search-results').classList.contains('none')) {
+				document.getElementById('second-search-results').classList.add('none');
+			}
+		}
+	});
+}
+
+async function SearchForPlace (id) {
+	id = id.length > 0 ? id + '-' : id;
+	const text = document.getElementById(id + 'searchbox').value;
+	if (text.length > 0) {
+		const url =
+			'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+			text +
+			'.json?fuzzyMatch=false&access_token=' +
+			mapboxgl.accessToken;
+		const response = await fetch(url, {
+			method : 'GET',
+		});
+		const data = await response.json();
+		loadResults(data.features, id);
+	}
+}
+
+function loadResults (array, id) {
+	//show results box
+	const resultsBox = document.getElementById(id + 'search-results');
+	resultsBox.innerHTML = '';
+
+	if (resultsBox.classList.contains('none')) {
+		resultsBox.classList.remove('none');
+	}
+
+	//add results into box
+	array.forEach(function (item) {
+		const name = document.createElement('div');
+		name.classList.add('name');
+		name.innerHTML = item.text;
+
+		const address = document.createElement('div');
+		address.classList.add('address');
+		address.innerHTML = item.place_name;
+
+		const searchitem = document.createElement('div');
+		searchitem.classList.add('search-item');
+		searchitem.appendChild(name);
+		searchitem.appendChild(address);
+		searchitem.addEventListener('click', function () {
+			flyToPlace(item.center);
+			if (!document.getElementById('search-results').classList.contains('none')) {
+				document.getElementById('search-results').classList.add('none');
+			}
+			if (!document.getElementById('second-search-results').classList.contains('none')) {
+				document.getElementById('second-search-results').classList.add('none');
+			}
+		});
+
+		resultsBox.appendChild(searchitem);
+	});
+}
+
+function GoToMyLocation () {
+	navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+		enableHighAccuracy : true,
+	});
+}
+
+function ToggleNavigator () {
+	const secondSearchbox = document.getElementById('secondSearch');
+	if (secondSearchbox.classList.contains('none')) {
+		secondSearchbox.classList.remove('none');
+
+		const searchbox = document.getElementById('second-searchbox');
+		searchbox.addEventListener('keyup', function () {
+			SearchForPlace('second');
+		});
+		navigate = true;
+	}
+	else {
+		secondSearchbox.classList.add('none');
+		navigate = false;
+	}
 }
--- repositioning/script.js
+++ repositioning/script.js
@@ -6,26 +6,50 @@ function SetUpMoveable () {
 	//add event to each of them
 	items.forEach(item => {
 		DragItem(item);
+		const controls = document.getElementById('controls');
+		const slider = document.createElement('input');
+		slider.id = 'slider';
+		slider.type = 'range';
+		slider.min = 510;
+		slider.max = document.getElementById('img').naturalWidth;
+		slider.value = item.width;
+
+		controls.appendChild(slider);
+		slider.oninput = function () {
+			ResizeItem(slider);
+		};
+	});
+}
+
+function RemoveMoveable () {
+	//get items that are marked moveable
+	const items = [ ...document.getElementsByClassName('non-moveable') ];
+
+	//add event to each of them
+	items.forEach(item => {
+		item.onmousedown = null;
+		const slider = document.getElementById('slider');
+		slider.remove();
 	});
 }
 
 function DragItem (item) {
-  const parent = item.parentElement;
+	const parent = item.parentElement;
 
-  //set click listener
+	//set click listener
 	item.onmousedown = function (event) {
-    //turn on mouse down flag
-    this.isDown = true;
-    //get starting mouse coords
+		//turn on mouse down flag
+		this.isDown = true;
+		//get starting mouse coords
 		this.mouseOldX = event.clientX;
 		this.mouseOldY = event.clientY;
-    //get sizes of element and its parent's
-    this.height = this.offsetHeight;
-    this.width = this.offsetWidth;
-    this.parentHeight = parent.offsetHeight;
-    this.parentWidth = parent.offsetWidth;
-    
-    //set starting style if it isnt already a number
+		//get sizes of element and its parent's
+		this.height = this.offsetHeight;
+		this.width = this.offsetWidth;
+		this.parentHeight = parent.offsetHeight;
+		this.parentWidth = parent.offsetWidth;
+
+		//set starting style if it isnt already a number
 		if (isNaN(parseInt(this.style.left))) {
 			this.style.left = this.offsetLeft + 'px';
 		}
@@ -34,49 +58,96 @@ function DragItem (item) {
 		}
 	};
 
-  //set mouse move listener
+	//set mouse move listener
 	item.onmousemove = function (event) {
-    //if mouse down flag is true = mouse is dragging
+		//if mouse down flag is true = mouse is dragging
 		if (this.isDown) {
-      //calculate the length of mouse's path
+			//calculate the length of mouse's path
 			const dX = event.clientX - this.mouseOldX,
-				    dY = event.clientY - this.mouseOldY;
+				dY = event.clientY - this.mouseOldY;
 
-      //calculate new coords of element
-      let newLeft = parseInt(this.style.left) + dX;
-      let newTop = parseInt(this.style.top) + dY;
-      
-      //if it's out of bounds horizontally
-      if(newLeft > 0){
-        newLeft = 0;
-      }
-      else if(newLeft < -(this.width - this.parentWidth)){
-        newLeft = -(this.width - this.parentWidth);
-      }
+			//calculate new coords of element
+			let newLeft = parseInt(this.style.left) + dX;
+			let newTop = parseInt(this.style.top) + dY;
 
-      //if it's out of bounds vertically
-      if(newTop > 0){
-        newTop = 0;
-      }
-      else if(newTop < -(this.height - this.parentHeight)){
-        newTop = -(this.height - this.parentHeight);
-      }
+			//if it's out of bounds horizontally
+			if (newLeft > 0) {
+				newLeft = 0;
+			}
+			else if (newLeft < -(this.width - this.parentWidth)) {
+				newLeft = -(this.width - this.parentWidth);
+			}
 
-      //apply the new position
+			//if it's out of bounds vertically
+			if (newTop > 0) {
+				newTop = 0;
+			}
+			else if (newTop < -(this.height - this.parentHeight)) {
+				newTop = -(this.height - this.parentHeight);
+			}
+
+			//apply the new position
 			this.style.left = newLeft + 'px';
 			this.style.top = newTop + 'px';
 
-      //reset the starting mouse coords to the current coords
+			//reset the starting mouse coords to the current coords
 			this.mouseOldX = event.clientX;
 			this.mouseOldY = event.clientY;
 		}
 	};
 
-  //set mouse up listener
+	//set mouse up listener
 	item.onmouseup = function (event) {
-    //remove mouse down flag
+		//remove mouse down flag
 		this.isDown = false;
 	};
 }
 
-SetUpMoveable();
+function ResizeItem (item) {
+	//console.log(item.value);
+	const img = document.getElementById('img');
+	const parent = img.parentElement;
+	const wrapper = parent.parentElement;
+	img.setAttribute('width', item.value);
+	//console.log(parent);
+
+	let newLeft = parseInt(parent.style.left);
+	let newTop = parseInt(parent.style.top);
+
+	//if it's out of bounds horizontally
+	if (newLeft > 0) {
+		newLeft = 0;
+	}
+	else if (newLeft < -(parent.offsetWidth - wrapper.offsetWidth)) {
+		newLeft = -(parent.offsetWidth - wrapper.offsetWidth);
+	}
+
+	//if it's out of bounds vertically
+	if (newTop > 0) {
+		newTop = 0;
+	}
+	else if (newTop < -(parent.offsetHeight - wrapper.offsetHeight)) {
+		newTop = -(parent.offsetHeight - wrapper.offsetHeight);
+	}
+
+	//apply the new position
+	parent.style.left = newLeft + 'px';
+	parent.style.top = newTop + 'px';
+
+	// console.log(parent.width, wrapper.offsetWidth, newLeft);
+}
+
+function ToggleReposition () {
+	if (document.getElementsByName('toMove')[0].classList.contains('moveable')) {
+		document.getElementsByName('toMove')[0].classList.remove('moveable');
+		document.getElementsByName('toMove')[0].classList.add('non-moveable');
+		RemoveMoveable();
+		document.getElementById('reposition').innerHTML = 'Reposition';
+	}
+	else {
+		document.getElementsByName('toMove')[0].classList.add('moveable');
+		document.getElementsByName('toMove')[0].classList.remove('non-moveable');
+		SetUpMoveable();
+		document.getElementById('reposition').innerHTML = 'Stop';
+	}
+}
